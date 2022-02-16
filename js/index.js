2476cfa6818ac36a01b9cb7fbb157c5eb72cace8
SeonghakHong <sung3045@naver.com>
2022-02-16 21:34:20 +0900

    js is fixed!!!!

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 const body = document.querySelector('body');
 const menuLi = document.querySelectorAll('#menu div');
-const sec = document.querySelectorAll('section');
-const getSectionHeight = sec[0].offsetHeight;
-console.log(getSectionHeight);
+const getBodyHeight = body.offsetHeight;
+console.log(getBodyHeight);
 // console.dir(menuLi[0]);
 
 // ***메뉴버튼 클릭시 해당 섹션으로 이동
@@ -11,12 +10,12 @@ for (i = 0; i < menuLi.length; i++) {
     if (event.target.id == 'home-btn') // 홈 버튼 클릭시
       window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
     else if (event.target.id == 'notice-btn') // 안내문 버튼 클릭시
-      window.scrollTo({left: 0, top: getSectionHeight, behavior: 'smooth'});
+      window.scrollTo({left: 0, top: (getBodyHeight / 5) * 1, behavior: 'smooth'});
     else if (event.target.id == 'ani-btn') //애니메이션 버튼 클릭시
-      window.scrollTo({left: 0, top: getSectionHeight * 2, behavior: 'smooth'});
+      window.scrollTo({left: 0, top: (getBodyHeight / 5) * 2, behavior: 'smooth'});
     else if (event.target.id == 'letter-btn') //초대장 버튼 클릭시
-      window.scrollTo({left: 0, top: getSectionHeight * 3, behavior: 'smooth'});
+      window.scrollTo({left: 0, top: (getBodyHeight / 5) * 3, behavior: 'smooth'});
     else if (event.target.id == 'contact-btn') //문의 버튼 클릭시
-      window.scrollTo({left: 0, top: getSectionHeight * 4, behavior: 'smooth'});
+      window.scrollTo({left: 0, top: getBodyHeight, behavior: 'smooth'});
   });
 }
