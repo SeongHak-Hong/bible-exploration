fad9cac72200b2a61d6868c022aa4f1e367a88fc
SeonghakHong <sung3045@naver.com>
2022-02-18 19:18:52 +0900

    mobile mediaquery

--- js/index.js
+++ js/index.js
@@ -10,11 +10,11 @@ window.addEventListener('DOMContentLoaded', function() {
     menuLi[i].addEventListener('click', function(event) {
       if (event.target.id == 'home-btn') // 홈 버튼 클릭시
         window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-      else if (event.target.id == 'notice-btn') // 안내문 버튼 클릭시
+      else if (event.target.id == 'letter-btn') // 안내문 버튼 클릭시
         window.scrollTo({left: 0, top: (getBodyHeight / 5) * 1, behavior: 'smooth'});
       else if (event.target.id == 'ani-btn') //애니메이션 버튼 클릭시
         window.scrollTo({left: 0, top: (getBodyHeight / 5) * 2, behavior: 'smooth'});
-      else if (event.target.id == 'letter-btn') //초대장 버튼 클릭시
+      else if (event.target.id == 'notice-btn') //초대장 버튼 클릭시
         window.scrollTo({left: 0, top: (getBodyHeight / 5) * 3, behavior: 'smooth'});
       else if (event.target.id == 'contact-btn') //문의 버튼 클릭시
         window.scrollTo({left: 0, top: getBodyHeight, behavior: 'smooth'});
@@ -27,6 +27,7 @@ window.addEventListener('DOMContentLoaded', function() {
   const letterPaper = document.querySelector('#letter-paper');
   const letterText = document.querySelector('#letter-text');
   const closeBtn = document.querySelector('#close-btn');
+  const firework = document.querySelector('.pyro');
 
   letterText.style.display = "block";
   letterPaper.style.display = "none";
@@ -44,6 +45,7 @@ window.addEventListener('DOMContentLoaded', function() {
         letterPaper.style.display = "block";
         letterPaper.style.opacity = 1;
         letterText.style.zIndex = -1;
+        firework.style.display = 'block';
       }, 800);
       
     }
@@ -54,6 +56,7 @@ window.addEventListener('DOMContentLoaded', function() {
     letterText.style.zIndex = 1;
     letter2.classList.add('letter2-ani');
     letter2.classList.remove('letter2-open');
+    firework.style.display = 'none';
   });
 });
 
